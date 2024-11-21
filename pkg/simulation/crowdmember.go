f4d6b945a068e67a29265b74f676f79849745b52
gmiralle <gautier.miralles@etu.utc.fr>
2024-11-21 23:06:26 +0100

    MAJ déplacement membre de foule, et j'ai retiré le nombre de voisin en arguement de Update health, faut implémenter les communications avec la map pour régler ça

--- pkg/simulation/crowdmember.go
+++ pkg/simulation/crowdmember.go
@@ -29,27 +29,45 @@ func NewCrowdMember(id int, position models.Position, distressProbability float6
 
 // Move randomly updates the CrowdMember's position
 func (c *CrowdMember) MoveRandom() {
+	if c.Position.X == -1 && c.Position.Y == -1 {
+		//The Member is Dead, so no moving
+		return
+
+	}
+
 	c.Position.X += float64(rand.Intn(3) - 1) // Move randomly in X direction (-1, 0, 1)
 	c.Position.Y += float64(rand.Intn(3) - 1) // Move randomly in Y direction (-1, 0, 1)
 }
 
-// MoveTo updates the CrowdMember's position towards a target position
+// MoveTo updates the CrowdMember's position towards a target position, at random speed
 func (c *CrowdMember) MoveTo(position models.Position) {
+
+	if c.Position.X == -1 && c.Position.Y == -1 {
+		//The Member is Dead, so no moving
+		return
+
+	}
+
+	speed := rand.Float64()
 	if c.Position.X < position.X {
-		c.Position.X++
+		c.Position.X += speed
 	} else if c.Position.X > position.X {
-		c.Position.X--
+		c.Position.X -= speed
 	}
 
 	if c.Position.Y < position.Y {
-		c.Position.Y++
+		c.Position.Y += speed
 	} else if c.Position.Y > position.Y {
-		c.Position.Y--
+		c.Position.Y -= speed
 	}
 }
 
 // UpdateHealth updates the health state of the CrowdMember
-func (c *CrowdMember) UpdateHealth(neighborCount int) {
+func (c *CrowdMember) UpdateHealth() {
+
+	//TODO use a function to communicate with the global map to know how many people are around our crowdMember
+	neighborCount := 3
+
 	effectiveDistressProbability := c.DistressProbability * float64(neighborCount)
 
 	if rand.Float64() < effectiveDistressProbability {
@@ -99,4 +117,24 @@ func (c *CrowdMember) Die() {
 // IsAlive checks if the CrowdMember is still alive
 func (c *CrowdMember) IsAlive() bool {
 	return c.Position.X >= 0 && c.Position.Y >= 0
+}
+
+func (c *CrowdMember) Myturn() {
+
+	c.MoveRandom()
+
+	c.UpdateHealth()
+
+	return
+
+	//alternative :
+	//TODO si on veut que les humains se dirigent vers des points d'intérêts on aura besoin d'un attribut objectif en plus
+	//TODO trouver le bon facteur de taille de carte
+
+	//TODO implémenter les colisions avec les obstacles et les bords de map
+
+	//sizeofmap := 20.
+	//but := models.Position{rand.Float64()*sizeofmap, rand.Float64()*sizeofmap}
+	//c.MoveTo(but)
+	//return
 }
